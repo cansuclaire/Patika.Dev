da38cfab3a5138555bb2dfaf3acab45373a83639
Cansu Claire Cottrell <97338597+cansuclaire@users.noreply.github.com>
2022-09-07 10:14:20 +0300

    Create palindromsayıbulma.java

---
+++ palindromsayıbulma.java
@@ -0,0 +1,22 @@
+public class palindomSayi {
+
+    //Palindrom sayı olup olmadığını bulma
+
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number==reverseNumber)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(isPalindrom(12344321));
+    }
+}
