fc159df3c7f53fedbd852fbf68669508c9acac90
Cansu Claire Cottrell <97338597+cansuclaire@users.noreply.github.com>
2022-09-07 09:09:32 +0300

    Create artıkyıl.java

---
+++ artıkyıl.java
@@ -0,0 +1,50 @@
+
+
+import java.util.Scanner;
+
+/**
+ * artikYil
+ */
+public class artikYil {
+
+    public static void main(String[] args) {
+        boolean leapYear = false;
+        int year;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen yıl giriniz: ");
+        year = input.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0){
+                    leapYear = true;
+                }else{
+                    leapYear = false;
+                }
+            }else {
+                leapYear = true;
+            }
+        } else{
+            leapYear = false;
+        }
+
+        if (leapYear){
+            System.out.println(year + " yılı bir artık yıldır !");
+        }else{
+            System.out.println(year + " yılı bir artık yıl değildir !");
+        }
+    }
+}
+/* 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
+100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+
+Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir.
+Sadece 400'e tam olarak bölünebilenlerin artık yıl kabul edilmesinin nedeni, bir astronomik yılın 365,25 gün değil, yaklaşık olarak 365,242 gün olmasından kaynaklanan hatayı gidermektir.
+
+Senaryo
+
+Yıl Giriniz : 2020
+2020 bir artık yıldır !
+Yıl Giriniz : 2021
+2021 bir artık yıldır değildir ! */
