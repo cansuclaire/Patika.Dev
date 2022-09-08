d78e622d709e59573ffe7f82b7c24f64c74f4129
Cansu Claire Cottrell <97338597+cansuclaire@users.noreply.github.com>
2022-09-08 09:01:51 +0300

    Create modelvalidasyonu.cs

---
+++ modelvalidasyonu.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DbOperations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BookStoreDbContext>(options=>options.UseInMemoryDatabase(databaseName:"BookStoreDB"));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
+var app = builder.Build();
+
+using ( var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
