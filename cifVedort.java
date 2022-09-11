29cf4cd15be4fb44a415f37c877544705479cba5
Cansu Claire Cottrell <97338597+cansuclaire@users.noreply.github.com>
2022-09-11 15:58:36 +0300

    Create cifVedort.java

---
+++ cifVedort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+/**
+ * tekSayiGireneKadar
+ */
+public class cifVedort {
+
+    public static void main(String[] args) {
+        int m , toplam = 0;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.println(" sayi Gir : " );
+            m = input.nextInt();
+            if ( m % 2 == 0 && m % 4 == 0){
+
+                toplam +=m;
+                
+            }
+            
+        } while (m % 2 ==0);
+        System.out.println(" toplam =   " + toplam);
+       
+
+    }
+}
