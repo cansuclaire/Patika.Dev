24f7f6979b8ad79bc0972384a84f28d481f2a008
Cansu Claire Cottrell <97338597+cansuclaire@users.noreply.github.com>
2022-09-12 08:17:30 +0300

    Create mukemmel.java

---
+++ mukemmel.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * mukemmel
+ */
+public class mukemmel {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n,sum=0 ;
+
+        System.out.print("Mukemmel sayi sorgulamasi yapacaginiz sayi : ");
+        n= input.nextInt();
+
+        for(int i=n-1 ; i>=1 ; i--){
+
+            if(n%i==0){
+                sum+=i;
+            }
+
+        }
+
+        if(sum==n){
+
+            System.out.println("Sayinizi mukemmel sayidir.");
+        }else{
+            System.out.println("Sayi mukemmel degildir.");
+
+        }
+
+
+
+    }
+}
