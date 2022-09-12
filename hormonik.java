5b814e9b013946f46defa20f5cc3dfafd60a6da0
Cansu Claire Cottrell <97338597+cansuclaire@users.noreply.github.com>
2022-09-12 08:09:30 +0300

    Create hormonik.java

---
+++ hormonik.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/**
+ * hormonik
+ */
+public class hormonik {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi;
+        double harm = 0, unit = 0;
+
+        System.out.println("Sayi giriniz: ");
+        sayi = input.nextInt();
+
+        for (int i = 1; i <= sayi; i++){
+            unit = (1.0 / i);
+            harm += unit;
+        }
+
+        System.out.println(harm);
+    }
+}
