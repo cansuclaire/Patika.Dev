7e98af9e73b9bb0e0658105b34cf5c4e669148f3
Cansu Claire Cottrell <97338597+cansuclaire@users.noreply.github.com>
2022-09-12 08:27:18 +0300

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/**
+ * Fibonacci
+ */
+public class Fibonacci {
+
+    
+
+    public static void main(String[] args) {
+        int k,n1=0,n2=1,n3;
+ 
+         Scanner inp = new Scanner(System.in);
+         System.out.print("Eleman sayısını giriniz: ");
+         k = inp.nextInt();
+ 
+ 
+         for (int i = 2  ;i<=k;i++){
+             n3=n1+n2;
+             System.out.print(n1 + " + " +n2 + " = "+n3);
+             n1=n2;
+             n2=n3;
+             System.out.println();
+         }
+         
+     }
+ }
